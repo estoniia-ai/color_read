c87a0a2638cbd5274b7e57d4278f24a15ffa259d
Otto Mättas <ottomattas@gmail.com>
2023-08-18 22:13:05 +0300

    Create full color block

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ function fetchData() {
             parsedData.forEach(item => {
                 const li = document.createElement('li');
                 li.textContent = `User: ${item.userId}, Color: ${item.color}, Timestamp: ${item.timestamp}`;
+                const colorBox = document.createElement('div');
+                colorBox.style.width = '100vw'; // 100% of viewport width
+                colorBox.style.height = '100vh'; // 100% of viewport height
+                colorBox.style.backgroundColor = item.color; // Use the color code directly
+            
+                li.appendChild(colorBox); // Add the color box element to the list item
                 colorList.appendChild(li);
             });
         })
