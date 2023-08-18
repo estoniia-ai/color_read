15ac631b624cf174ef0539f8c92db9e3594023fe
Otto Mättas <ottomattas@gmail.com>
2023-08-18 21:55:29 +0300

    Add polling

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-fetch('https://kf3fcdvdmc.execute-api.eu-north-1.amazonaws.com/v2/color-read')
-    .then(response => response.json())
-    .then(data => {
-        const colorList = document.getElementById('colorList');
-        data.forEach(item => {
-            const li = document.createElement('li');
-            li.textContent = `User: ${item.userId}, Color: ${item.color}, Timestamp: ${item.timestamp}`;
-            colorList.appendChild(li);
+function fetchData() {
+    fetch('https://kf3fcdvdmc.execute-api.eu-north-1.amazonaws.com/v2/color-read')
+        .then(response => response.json())
+        .then(data => {
+            const colorList = document.getElementById('colorList');
+            
+            // Clear the existing list to avoid appending duplicate items
+            colorList.innerHTML = '';
+
+            data.forEach(item => {
+                const li = document.createElement('li');
+                li.textContent = `User: ${item.userId}, Color: ${item.color}, Timestamp: ${item.timestamp}`;
+                colorList.appendChild(li);
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
         });
-    })
-    .catch(error => {
-        console.error("Error fetching data:", error);
-    });
+}
+
+// Initial fetch
+fetchData();
+
+// Set up polling every second
+setInterval(fetchData, 1000);
